d90b26a0b6a48058435926d25c27e5b54d11904f
Lucas Yang <lucasmikan06@outlook.com>
2025-01-30 16:10:20 -0500

    commit

--- DjangoProject/TestFile/views.py
+++ DjangoProject/TestFile/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse, HttpResponse
+from .models import YourModel  # Replace with your actual model
+from django.views import View
 
-# Create your views here.
+# Function-based view
+def home(request):
+    return render(request, 'home.html')
+
+def get_data(request):
+    data = list(YourModel.objects.values())  # Convert queryset to list of dicts
+    return JsonResponse({"data": data})
+
+# Class-based view
+class DetailView(View):
+    def get(self, request, pk):
+        obj = get_object_or_404(YourModel, pk=pk)
+        return render(request, 'detail.html', {'object': obj})
