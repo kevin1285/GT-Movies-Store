22523578e99ee99684b752bdc194b690f071edce
kevin1285 <132208181+kevin1285@users.noreply.github.com>
2025-01-30 16:37:29 -0500

    delete everything

--- DjangoProject/moviesStore/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestfileConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "moviesStore"
--- README.md
+++
@@ -1,5 +0,0 @@
-# CS-2340-Ian3
-
-Hello - Patrick
-
-おはよう　ーLucas
