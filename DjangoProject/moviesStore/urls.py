6c68f4c12875d4958715efd43227099ec1d60b4c
thomasg13 <thomasguo13@gmail.com>
2025-01-31 16:46:43 -0500

    Add about page and create base template

--- DjangoProject/moviesStore/urls.py
+++ DjangoProject/moviesStore/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import home
+from .views import home, about
 
 urlpatterns = [
     path('', home, name='home'),
+    path('about', about, name='about'),
 ]
