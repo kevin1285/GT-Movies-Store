9bba83b4a11b087c133b0010c54949cd4f280ac6
thomasg13 <thomasguo13@gmail.com>
2025-02-04 17:06:24 -0500

    Fixed login, dont need moviesStore anymore

--- DjangoProject/moviesStore/urls.py
+++ DjangoProject/moviesStore/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from .views import home, login_view, logout_view, signup_view
 
 urlpatterns = [
-    path('', home, name='home'),
+    #path('', home, name='home'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('signup/', signup_view, name='signup'),
