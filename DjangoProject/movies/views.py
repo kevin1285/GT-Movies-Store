dcc1187e50c1fe7e257d1b0ba93f44aac4b87f09
thomasg13 <thomasguo13@gmail.com>
2025-02-05 19:43:18 -0500

    another small bug

--- DjangoProject/movies/views.py
+++ DjangoProject/movies/views.py
@@ -46,7 +46,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('home:home')  # Redirect after successful login
+            return redirect('home:movie_list')  # Redirect after successful login
         else:
             messages.error(request, "Invalid username or password")
 
