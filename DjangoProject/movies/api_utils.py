df58699b41e3343450bbd65800e5f224510a2ba2
kevin1285 <132208181+kevin1285@users.noreply.github.com>
2025-02-08 16:17:34 -0500

    change movies to card view in grid

--- DjangoProject/movies/api_utils.py
+++ DjangoProject/movies/api_utils.py
@@ -7,10 +7,18 @@ def get_popular_movies():
     url = f"{BASE_URL}/movie/popular"
     params = {"api_key": settings.TIMB_API_KEY, "language": "en-US","page":1}
     response = requests.get(url, params=params)
-    print("API Status Code: ", response.status_code)
+    #print("API Status Code: ", response.status_code)
     if response.status_code == 200:
         data = response.json()
-        print("API Response:", data)
+        #print("API Response:", data.get("results", []))
         return data.get("results",[])
-    print("error fetching:", response.text)
+    #print("error fetching:", response.text)
     return []
+
+def get_movie(movie_id):
+    url = f"{BASE_URL}/movie/{movie_id}"
+    params = {"api_key": settings.TIMB_API_KEY, "language": "en-US","page":1}
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        return response.json()
+    return None
--- DjangoProject/movies/views.py
+++ DjangoProject/movies/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from movies.api_utils import get_popular_movies
-
+from movies.api_utils import get_popular_movies, get_movie
 
 def movie_list(request):
     movies = get_popular_movies()
     print(movies)
     return render(request, "movies/movie_list.html", {"movies":movies})
+
+def movie(request, movie_id):
+    movie = get_movie(movie_id)
+    return render(request, "movies/movie.html", {"movie":movie})
 
 
