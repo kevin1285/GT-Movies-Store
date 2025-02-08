4e884a7bcd86f4a5ac25e832d5cb990171d45994
kevin1285 <132208181+kevin1285@users.noreply.github.com>
2025-02-08 11:33:00 -0500

    deleted unneeded stuff :)

--- DjangoProject/home/urls.py
+++ DjangoProject/home/urls.py
@@ -6,6 +6,4 @@ app_name = 'home'  # This enables namespacing in templates
 urlpatterns = [
     path("", views.home, name='index'),
     path("about/", views.about, name='about'),
-    # path('home/', views.home, name='home'),
-    # path('about/', views.about, name='about'),
 ]
--- DjangoProject/home/views.py
+++ DjangoProject/home/views.py
@@ -3,10 +3,6 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home/home.html')
 
-def index(request):
-    template_data = {}
-    template_data['title'] = 'Movies Store'
-    return render(request, 'home/index.html', {'template_data': template_data})
 def about(request):
     template_data = {}
     template_data['title'] = 'About'
