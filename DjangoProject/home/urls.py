aac150dc5e6fca42faa5135ea32c519d67f001ae
kevin1285 <132208181+kevin1285@users.noreply.github.com>
2025-02-08 11:19:33 -0500

    refactor (change movies views from home to movies file)

--- DjangoProject/home/urls.py
+++ DjangoProject/home/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
 from . import views
-from .views import movie_list
-from .views import home, about
 
 app_name = 'home'  # This enables namespacing in templates
 
 urlpatterns = [
-    path('home/', views.movie_list, name='movie_list'),
-
-    path("", home, name='index'),
-    path("about/", about, name='about'),
+    path("", views.home, name='index'),
+    path("about/", views.about, name='about'),
     # path('home/', views.home, name='home'),
     # path('about/', views.about, name='about'),
 ]
