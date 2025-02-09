ebd0b975121b772cc9561397071dd4912841776c
kevin1285 <132208181+kevin1285@users.noreply.github.com>
2025-02-09 09:51:18 -0500

    add reviews model and define form

--- DjangoProject/movies/models.py
+++ DjangoProject/movies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Review(models.Model):
+    movie_id = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # if user is deleted, so are its reviews
+    text = models.TextField()
+    rating = models.IntegerField(choices=[(1,1) for i in range(1,6)])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.rating}-star review by {self.user.username} for movie {self.movie_id}: {self.text}"
+
+
