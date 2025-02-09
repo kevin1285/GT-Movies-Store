cd617f89489760b59a1b551a1c118f4c2bd5ef0b
kevin1285 <132208181+kevin1285@users.noreply.github.com>
2025-02-09 11:48:37 -0500

    add reviews (see, write)

--- DjangoProject/movies/models.py
+++ DjangoProject/movies/models.py
@@ -5,7 +5,7 @@ class Review(models.Model):
     movie_id = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE) # if user is deleted, so are its reviews
     text = models.TextField()
-    rating = models.IntegerField(choices=[(1,1) for i in range(1,6)])
+    rating = models.IntegerField(choices=[(i, i) for i in range(1,11)])
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
