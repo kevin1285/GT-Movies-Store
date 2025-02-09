5856f2a4daead3cb11bdaa695ae24fcf880a4418
kevin1285 <132208181+kevin1285@users.noreply.github.com>
2025-02-09 13:51:52 -0500

    switch rating and text order

--- DjangoProject/movies/forms.py
+++ DjangoProject/movies/forms.py
@@ -4,8 +4,8 @@ from .models import Review
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ['text', 'rating']
+        fields = ['rating', 'text']
         widgets = {
-            'text': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
             'rating': forms.Select(attrs={'class': 'form-control'}),
+            'text': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
         }
