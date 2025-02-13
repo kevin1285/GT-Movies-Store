9cfef49c905a69605c10162e764d1000c5aabb79
kevin1285 <132208181+kevin1285@users.noreply.github.com>
2025-02-13 11:53:13 -0500

    create blank cart page

--- DjangoProject/movies/urls.py
+++ DjangoProject/movies/urls.py
@@ -4,11 +4,16 @@ from . import views
 
 app_name = 'movies'
 urlpatterns = [
+    # MOVIES
     path('', views.movie_list, name='movie_list'),
     path('<int:movie_id>/', views.movie, name='movie'),
-    path('<int:movie_id>/submit_review/', views.submit_review, name='submit_review'),
 
-    #backend-only:
+    # REVIEWS
+    path('<int:movie_id>/submit_review/', views.submit_review, name='submit_review'),
     path('<int:review_id>/delete_review/', views.delete_review, name='delete_review'),
     path('<int:review_id>/edit_review/', views.edit_review, name='edit_review'),
+
+    # SHOPPING CART
+    path('cart/', views.cart, name='cart'),
+
 ]
