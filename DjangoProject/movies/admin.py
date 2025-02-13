3bc50daa90669539e6c938725db148383dccad9c
thomasg13 <thomasguo13@gmail.com>
2025-02-13 16:32:13 -0500

    migrate + show cart in admin

--- DjangoProject/movies/admin.py
+++ DjangoProject/movies/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from movies.models import Movie, Review
+from movies.models import Movie, Review, Cart
 
 # Register your models here.
 
 admin.site.register(Movie)
 admin.site.register(Review)
+admin.site.register(Cart)
